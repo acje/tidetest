87cde668e15196589a1f0728d107da07553f74d9
Anders Jensen <anders.c.jensen@gmail.com>
2019-11-30 11:11:42 +0100

    example works now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-#[async_std::main]
-async fn main() -> Result<(), std::io::Error> {
-    let mut app = tide::new();
-    app.at("/").get(|_| async move { "Hello, world!" });
-    app.listen("0.0.0.0:8000").await?;
-    Ok(())
+use async_std::task;
+
+fn main() -> Result<(), std::io::Error> {
+    task::block_on(async {
+        let mut app = tide::new();
+        app.at("/").get(|_| async move { "Hello, world!" });
+        app.listen("0.0.0.0:8000").await?;
+        Ok(())
+    })
 }
