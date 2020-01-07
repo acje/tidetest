17b20df1256fb7b4bed9b6433caf9d675c3bbd6b
Anders <anders.c.jensen@gmail.com>
2020-01-07 06:05:32 -0800

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use async_std::task;
 fn main() -> Result<(), std::io::Error> {
     task::block_on(async {
         let mut app = tide::new();
+       // app.at("/").get(|req| async move { req });
         app.at("/").get(|_| async move { "Hello, world!" });
         app.listen("0.0.0.0:8000").await?;
         Ok(())
